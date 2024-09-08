2fb81d2fecd5ceb3f48005eea09de2236243ec4d
HanchinamaniHrithik <hrithikhanchinamani@gmail.com>
2024-09-09 02:42:44 +0530

    Done with CRUD, not gonna do frontend work. Might restructure app for optimization

--- Starter/Controllers/tasks.js
+++ Starter/Controllers/tasks.js
@@ -11,7 +11,7 @@ const getalltasks = async (req, res) => {
 
 const Createtask = async (req, res) => {
     const anottask = await taskimported.create(req.body)/*   ***READ THISSSSSS***** You can use try&catch method here
-    When validation fails you wont see the error in postman/DB but only in your server(console)
+    When validators(Value, default,trim, etc) fails you wont see the error in postman/DB but only in your server(console)
      to change this, Try&catch will help you display the error 
      try -> await-> if error -> catch error*/
     res.status(201).json({ anottask }) // if err -> status(500).json{msg:(error) or type up short text}
